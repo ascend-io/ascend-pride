50908ed67500dbbd363fcef149ddf87f95aef9ae
Shifra Isaacs <shifra@ascend.io>
2025-06-24 23:24:35 +0000

    Add new SQL component 'sales_stores.sql' selecting distinct store sales data with a not_null test on the timestamp column.

---
+++ agentic_demo/flows/transform-demo/components/sales_stores.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT
+    STORE_ID,
+    ASCENDER_ID,
+    PRICE,
+    QUANTITY,
+    TIMESTAMP,
+    ID
+FROM {{ ref('read_sales_stores', flow='extract-load') }}
+
+{{ with_test("not_null", column="timestamp") }}
