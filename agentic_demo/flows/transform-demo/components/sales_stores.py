74227797f487cf35ab0879dde5b96f5742335b93
Shifra Isaacs <shifra@ascend.io>
2025-06-24 23:09:22 +0000

    Rewrite sales_stores component from Python Ibis transform to a raw SQL query selecting distinct rows from the read_sales_stores reference.

--- agentic_demo/flows/transform-demo/components/sales_stores.py
+++ agentic_demo/flows/transform-demo/components/sales_stores.py
@@ -1,20 +1,8 @@
-import ibis
-
-from ascend.resources import ref, transform, test
-from ascend.application.context import ComponentExecutionContext
-
-@transform(
-    inputs=[ref("read_sales_stores", flow="extract-load")],
-    materialized="table",
-    tests=[test("not_null", column="timestamp")],
-)
-def sales_stores(
-    read_sales_stores: ibis.Table, context: ComponentExecutionContext
-) -> ibis.Table:
-    # Inline the cleaning logic
-    if ibis.get_backend(read_sales_stores).name == "snowflake":
-        cleaned = read_sales_stores.rename("ALL_CAPS").distinct()
-    else:
-        cleaned = read_sales_stores.rename("snake_case").distinct()
-    return cleaned
-
+SELECT DISTINCT
+    STORE_ID,
+    ASCENDER_ID,
+    PRICE,
+    QUANTITY,
+    TIMESTAMP,
+    ID
+FROM {{ ref('read_sales_stores', flow='extract-load') }}
